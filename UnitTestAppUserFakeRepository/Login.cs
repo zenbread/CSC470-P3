04eaaa1f7c1cd68414d7ff908917bc4ea57539b0
nick <nmcamp87@gmail.com>
2021-09-22 21:02:30 -0400

    Added both test cases

--- UnitTestAppUserFakeRepository/Login.cs
+++ UnitTestAppUserFakeRepository/Login.cs
@@ -9,15 +9,26 @@ namespace UnitTestAppUserFakeRepository
         [TestMethod]
         public void LoginWithCorrectCredentialsSuccessful()
         {
-            //Arange
+            // Arrange
             FakeAppUserRepository repo = new FakeAppUserRepository();
-            //Act
-            //Assert
+            // Act
+            bool response = repo.Login("Steve1234", "Billy");
+            // Assert
+            Assert.IsTrue(response);
+            Assert.AreEqual(true, repo.GetByUserName("Steve1234").isAuthenticated);
         }
-
+        
         [TestMethod]
         public void LoginWithIncorrectCredentialsFail()
         {
+            // Arrange
+            FakeAppUserRepository repo = new FakeAppUserRepository();
+            // Act
+            bool response = repo.Login("Steve1234", "Billy1234");
+            // Assert
+            Assert.IsFalse(response);
+            Assert.AreEqual(false, repo.GetByUserName("Steve1234").isAuthenticated);
+
         }
     }
 }
