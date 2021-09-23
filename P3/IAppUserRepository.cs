347e5b4224dcdce6497914a42ab96d6b9c66ee89
nick <nmcamp87@gmail.com>
2021-09-22 20:50:38 -0400

    Added interface

---
+++ P3/IAppUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3
+{
+    interface IAppUserRepository
+    {
+        bool Login(string UserName, string Password);
+        List<AppUser> GetAll();
+        void SetAuthentication(string UserName, bool IsAuthenticated);
+        AppUser GetByUserName(string UserName);
+    }
+}
